d4bc21670e98e9798b3e51f785ecc383871138ff
Bastler1971 <Dac362@gmx.de>
2020-12-17 21:58:56 +0100

    Update Configuration_adv.h

--- Configuration_adv.h
+++ Configuration_adv.h
@@ -474,7 +474,7 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN PB0 //HTG -1
+#define E0_AUTO_FAN_PIN PB0 //Bastler1971 -1
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
@@ -1650,12 +1650,12 @@
  *
  * See https://marlinfw.org/docs/features/lin_advance.html for full instructions.
  */
-#define LIN_ADVANCE //HTG
+#define LIN_ADVANCE //Bastler1971
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
   #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
   #define LA_DEBUG            // If enabled, this will generate debug information output over USB.
-  #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration //HTG
+  #define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration //Bastler1971
 #endif
 
 // @section leveling
